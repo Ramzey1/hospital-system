1f10e4ab1a773d023c2628b1074d71f89ed52770
Ramzey <ramzey.elhaddad@yahoo.co.uk>
2018-09-23 13:37:24 +0100

    integrate jquery

--- frontend/src/src/App.js
+++ frontend/src/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
+
+  state = {
+    message: ""
+  };
+
+  componentDidMount() {
+    $.ajax({
+      "url": "",
+      method: "GET",
+      data: {},
+      "success": function(data) {
+        this.setState({message: data});
+      }
+
+    });
+  }
+  
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.state.message}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
